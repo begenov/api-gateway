1039a72b5c94fead787a7b900c5e04e7dace84dd
begenov <begenovorazali@icloud.com>
2023-07-06 14:29:36 +0600

    init register service

---
+++ internal/client/register_client.go
@@ -0,0 +1,40 @@
+package client
+
+import (
+	"context"
+
+	"github.com/begenov/api-gateway/pb"
+	"google.golang.org/grpc"
+)
+
+type RegisterServiceClient struct {
+	Conn   *grpc.ClientConn
+	Client pb.RegisterClient
+}
+
+func NewRegisterServiceClient(addr string) (*RegisterServiceClient, error) {
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+
+	// Создайте клиентский объект
+	client := pb.NewRegisterClient(conn)
+
+	return &RegisterServiceClient{
+		Conn:   conn,
+		Client: client,
+	}, nil
+}
+
+func (c *RegisterServiceClient) SignUp(ctx context.Context, req *pb.RequestRegister) (*pb.Response, error) {
+	return c.Client.SignUp(ctx, req)
+}
+
+func (c *RegisterServiceClient) SignIn(ctx context.Context, req *pb.RequestSignIn) (*pb.ResponseToken, error) {
+	return c.Client.SignIn(ctx, req)
+}
+
+func (c *RegisterServiceClient) RefreshToken(ctx context.Context, req *pb.RequestToken) (*pb.ResponseToken, error) {
+	return c.Client.RefreshToken(ctx, req)
+}
