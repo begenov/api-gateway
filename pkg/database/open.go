3ff76b9583b6222e8aaed19bc71edcb4f03bc4b9
begenov <begenovorazali@icloud.com>
2023-07-06 14:35:59 +0600

    init database

---
+++ pkg/database/open.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/begenov/api-gateway/internal/config"
+)
+
+func Open(ctx context.Context, cfg config.PostgresConfig) (*sql.DB, error) {
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%d/%s?sslmode=disable", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Database)
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
